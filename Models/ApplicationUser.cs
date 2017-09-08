89316d07957436b43d0093f2c48f09b8f9d689ac
tenorjas <jonathan.a.schultz@gmail.com>
2017-09-08 16:55:30 -0400

    trying to create models

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -9,5 +9,9 @@ namespace stackoverflow.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public int ID {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        
     }
 }
